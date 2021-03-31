633e872d210cc4162d8ec95794460912177d9997
khai96_ <hvksmr1996@gmail.com>
2021-03-31 22:38:28 +0700

    Create codegen.ts

---
+++ scripts/lib/codegen.ts
@@ -0,0 +1,33 @@
+import { encode } from './std/encoding/base64.ts'
+import { join } from './std/path.ts'
+import Artifact from './artifact.ts'
+import ROOT from './workspace.ts'
+
+export const code = (base64: string): string =>
+  [
+    '// sane-fmt-ignore',
+    `export const base64 = '${base64}'`,
+    'export default base64',
+    '',
+  ].join('\n')
+
+export class CodeGenerator<Version extends string> {
+  #artifact: Artifact<Version>
+
+  constructor(artifact: Artifact<Version>) {
+    this.#artifact = artifact
+  }
+
+  public readonly path = join(ROOT, 'lib', 'base64.ts')
+
+  public async generate() {
+    const data = await Deno
+      .readFile(this.#artifact.path)
+      .then(encode)
+      .then(code)
+
+    await Deno.writeTextFile(this.path, data)
+  }
+}
+
+export default CodeGenerator
