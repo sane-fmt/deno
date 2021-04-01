4892e62938d2687b764311aa91cd90ab1526f4d8
khai96_ <hvksmr1996@gmail.com>
2021-04-01 08:47:30 +0700

    Create u8v

--- lib/deps.ts
+++ lib/deps.ts
@@ -1,3 +1,4 @@
+export { decode } from 'https://deno.land/std@0.91.0/encoding/base64.ts'
 export { default as Context } from 'https://deno.land/std@0.91.0/wasi/snapshot_preview1.ts'
 export type { ContextOptions } from 'https://deno.land/std@0.91.0/wasi/snapshot_preview1.ts'
 export * from 'https://deno.land/x/compose@1.3.2/index.js'
---
+++ lib/u8v.ts
@@ -0,0 +1,4 @@
+import base64 from './base64.ts'
+import { decode } from './deps.ts'
+export const u8v = decode(base64)
+export default u8v
