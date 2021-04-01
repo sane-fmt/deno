4b9effa73732edd53a35642a732f23e7ef3ce261
khai96_ <hvksmr1996@gmail.com>
2021-04-01 11:24:29 +0700

    Improve performance

--- lib/run.ts
+++ lib/run.ts
@@ -1,8 +1,10 @@
 import createContext from './context.ts'
 
 export async function run(blob: Uint8Array): Promise<number | null> {
-  const context = await createContext()
-  const module = await WebAssembly.compile(blob)
+  const [context, module] = await Promise.all([
+    createContext(),
+    WebAssembly.compile(blob),
+  ])
   const instance = await WebAssembly.instantiate(module, {
     wasi_snapshot_preview1: context.exports,
   })
