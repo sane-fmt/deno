6886721a20f5d37296b2da1d1264513a14dea7d0
khai96_ <hvksmr1996@gmail.com>
2021-04-02 14:39:54 +0700

    Skip preopens when --help, -h, --version, -V, or --stdio present

--- lib/preopens.ts
+++ lib/preopens.ts
@@ -1,19 +1,33 @@
 import { dirname } from './std/path.ts'
-import xArgs, { symbols } from './x/args.ts'
+import xArgs, { symbols, flags } from './x/args.ts'
 
-function argValues(args: readonly string[]) {
-  const res = xArgs.parse(args)
+function getTargetFiles(args: readonly string[]) {
+  const res = xArgs
+    .with(flags.BinaryFlag('help', { alias: ['h'] }))
+    .with(flags.BinaryFlag('version', { alias: ['V'] }))
+    .with(flags.BinaryFlag('stdio'))
+    .parse(args)
+
   if (res.tag !== symbols.MAIN_COMMAND) {
     throw new Error('Something goes wrong.')
   }
+
+  // when the following flags present, sane-fmt won't touch the filesystem,
+  // preopens become an unnecessary burden on performance
+  const { help, version, stdio } = res.value
+  if (help || version || stdio) return null
+
   return res.remaining().rawValues()
 }
 
 export async function preopens(args: readonly string[]): Promise<Record<string, string>> {
   const preopens: Record<string, string> = {}
 
+  const targetFiles = getTargetFiles(args)
+  if (!targetFiles) return preopens
+
   const directories = await Promise.all(
-    argValues(args).map(async name => {
+    targetFiles.map(async name => {
       try {
         const stats = await Deno.lstat(name)
         return stats.isDirectory ? name : dirname(name)
