b98bca4d52b8226be9aaf36b57a3628ced7d2b16
khai96_ <hvksmr1996@gmail.com>
2021-04-03 13:11:46 +0700

    Allow FileSystemTree to be a file

--- utils/fs-tree.ts
+++ utils/fs-tree.ts
@@ -1,27 +1,32 @@
 import { ensureFile } from './std/fs.ts'
 import { join } from './std/path.ts'
 
-export type FileSystemTree = {
-  readonly [path: string]: string | FileSystemTree
+type Directory = {
+  readonly [path: string]: File | Directory
 }
 
+type File = string
+
+export type FileSystemTree = File | Directory
+
 export async function buildFileSystemTree(tree: FileSystemTree, ...prefix: string[]) {
-  await Promise.all(
-    Object.entries(tree).map(async ([path, content]) => {
-      switch (typeof content) {
-        case 'object':
-          return buildFileSystemTree(content, ...prefix, path)
-        case 'string': {
-          const filename = join(...prefix, path)
-          await ensureFile(filename)
-          await Deno.writeTextFile(filename, content)
-          return
-        }
-        default:
-          throw new TypeError(`Unexpected type: ${typeof content}`)
-      }
-    }),
-  )
+  switch (typeof tree) {
+    case 'object':
+      await Promise.all(
+        Object
+          .entries(tree)
+          .map(([path, content]) => buildFileSystemTree(content, ...prefix, path)),
+      )
+      return
+    case 'string': {
+      const filename = join(...prefix)
+      await ensureFile(filename)
+      await Deno.writeTextFile(filename, tree)
+      return
+    }
+    default:
+      throw new TypeError(`Unexpected type: ${typeof tree}`)
+  }
 }
 
 export default buildFileSystemTree
