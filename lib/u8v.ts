5210eac106c510124dca5dc8ef33a5f3a8544e60
khai96_ <hvksmr1996@gmail.com>
2021-04-07 18:59:37 +0700

    refactor: Switch from std/encoding/base64.ts to Uint8Array.from
    reduce bundle size

--- lib/u8v.ts
+++ lib/u8v.ts
@@ -1,7 +1,6 @@
-import { decode } from './std/encoding/base64.ts'
 import base64 from './base64.js'
 
 /** Code of the `sane-fmt` program */
-export const u8v = decode(base64)
+export const u8v = Uint8Array.from(atob(base64), x => x.charCodeAt(0))
 
 export default u8v
